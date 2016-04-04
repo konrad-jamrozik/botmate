3c209260968f80598090a5956611279212f393cc
Konrad Jamrozik <spawarotti@jamro.pl>
2016-04-04 20:03:49 +0200

    add main() test stub

---
+++ demo/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,19 @@
+// Copyright (c) 2012-2016 Saarland University
+// All rights reserved.
+//
+// Author: Konrad Jamrozik, konrad.jamrozik@cispa.saarland
+//
+// This file is part of the "BotMate" project.
+//
+// github.com/konrad-jamrozik/botmate
+
+import org.junit.Test
+
+class MainKtTest {
+
+  @Test
+  fun mainTest() {
+    println("test!")
+    main(emptyArray())
+  }
+}
