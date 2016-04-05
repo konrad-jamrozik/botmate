1be26d2028f35805a5d4fa4c455c6d2d2d184f75
Konrad Jamrozik <spawarotti@jamro.pl>
2016-04-05 19:04:23 +0200

    add todo

--- demo/src/main/kotlin/com/github/konrad_jamrozik/botmate/demo/Robot.kt
+++ demo/src/main/kotlin/com/github/konrad_jamrozik/botmate/demo/Robot.kt
@@ -12,6 +12,8 @@ package com.github.konrad_jamrozik.botmate.demo
 class Robot : IRobot {
   override fun moveTo(coordinates: Pair<Int, Int>) {
     log.debug("Robot.moveTo()")
+    // KJA next: depend on 'controller' project, create RobotControllerAdapter() here and use it to communicate with the robot.
+    // For reference, see org.droidmate.robot.AndroidDeviceWithRobot#performActionOnDevice
   }
 
   val log = loggerFor(Robot::class.java)
